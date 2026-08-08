c6b69f0d7394d9b58ac64fa9c7d85cf6f0e1f786
Roshini-0211 <roshin0211@gmail.com>
2026-08-08 18:13:53 +0530

    Add Leapyear program to check if a year is a leap year

---
+++ Introduction/PracticeProblem/Leapyear.java
@@ -0,0 +1,31 @@
+package PracticeProblem;
+
+public class Leapyear {
+    public static void main (String[]args){
+        int year = 2015; 
+
+        if (year % 4 == 0){ 
+            if (year % 100 == 0){ 
+                if (year % 400 == 0){ 
+                    System.out.println(year + " is a leap year"); 
+                } else { 
+                    System.out.println(year + " is not a leap year"); 
+                } 
+            } else { 
+                System.out.println(year + " is a leap year"); 
+            } 
+        } else { 
+            // FIX: This handling was missing! 
+            // If a year is not divisible by 4, it can never be a leap year.
+            System.out.println(year + " is not a leap year"); 
+        } 
+           /*if((year%4==0 && year%100!=0 )|| year%400==0){
+            System.out.println(year +" is a leap year");
+           }else{
+            System.out.println(year +" is not a leap year");
+           }*/
+    
+    
+}
+}
+
