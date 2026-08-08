f3ccf8e4872bf12ef805c2bf7c4b7b6f4d0e8e11
Roshini-0211 <roshin0211@gmail.com>
2026-08-08 18:13:25 +0530

    Add CircleArea program to calculate the area of a circle

---
+++ Introduction/PracticeProblem/CircleArea.java
@@ -0,0 +1,11 @@
+package PracticeProblem;
+
+public class CircleArea {
+    
+    public static void main(String[] args) {
+        float radius=3.5f;
+    double area =3.14*radius*radius;
+        System.out.println("Area of Circle is : " + area);
+        System.out.printf("Area of Circle is : %.2f",area); 
+    }
+}
