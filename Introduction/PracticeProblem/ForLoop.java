dd1f561d6dc890ba15b76679251fe2c4115abe92
Roshini-0211 <roshin0211@gmail.com>
2026-08-08 18:13:35 +0530

    Add ForLoop program to print good morning 100 times

---
+++ Introduction/PracticeProblem/ForLoop.java
@@ -0,0 +1,12 @@
+package PracticeProblem;
+
+public class ForLoop {
+    public static void main(String[]args){
+        int n=100;
+        for(int i=1;i<=n;i++){
+           
+            System.out.println(i+" good morning");
+        }
+    }
+    
+}
