09380fb1d500aae0d184e28fa4a2bf2182cc36e7
Roshini-0211 <roshin0211@gmail.com>
2026-08-10 20:18:49 +0530

    Add Prime program to check if a number is prime

---
+++ Introduction/PracticeProblem/Prime.java
@@ -0,0 +1,27 @@
+package PracticeProblem;
+
+public class Prime {
+    public static void main(String[] args) {
+        
+        int num = 29;
+        boolean isPrime = true;
+
+        for (int i = 2; i <= num-1; i++) {
+            
+            System.out.println(i);
+            if (num % i == 0) {
+                isPrime = false;
+                //break;
+            }
+        }
+
+        if (isPrime) {
+        
+
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+    
+}
