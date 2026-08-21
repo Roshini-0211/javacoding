60fcd109f36fa66b645f570e13b4ca8f30bbb365
Roshini-0211 <roshin0211@gmail.com>
2026-08-21 20:10:38 +0530

    Add StringCOMPARISON: demonstrate string comparison using ==, equals, and equalsIgnoreCase

---
+++ Introduction/String/StringCOMPARISON.java
@@ -0,0 +1,11 @@
+package String;
+
+public class StringCOMPARISON {
+    public static void main(String[] args) {
+        //"=="  ".equals()"
+        String s1=new String("ppoja");
+        String s2="pooja";
+        System.out.println(s1+s2);
+    }
+    
+}
