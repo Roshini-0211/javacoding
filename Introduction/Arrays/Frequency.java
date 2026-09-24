5ebbad6c2376a2ad1bd6424341b7c4f1e5ea20a1
Roshini-0211 <roshin0211@gmail.com>
2026-09-24 21:31:47 +0530

    Update frequency counting and majority detection

--- Introduction/Arrays/Frequency.java
+++ Introduction/Arrays/Frequency.java
@@ -5,9 +5,10 @@ public class Frequency {
         int arr[]={10,30,10,30,30,15};
         int len=arr.length;
         boolean isvisited[]=new boolean[len];
+        int count=1;
         for(int i=0;i<len;i++){
             if(isvisited[i]==false){
-                int count=1;
+                //int count=1;
                 isvisited[i]=true;
                 for(int j=i+1;j<len;j++){
                     if(arr[i]==arr[j]){
@@ -17,8 +18,15 @@ public class Frequency {
                     }
                     
                 }
+                //majority
+                //if (count>(len/2)){
+                //    System.out.println(arr[i]);
+                //}
 System.out.println(arr[i]+"=>"+count);
             }
+            if (count>(len/2)){
+                    System.out.println(arr[i]);
+                }
         }
     }
     
