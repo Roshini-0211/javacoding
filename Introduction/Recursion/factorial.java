d3dab42e04947213f464d50a7e98ebeb77f8fced
Roshini-0211 <roshin0211@gmail.com>
2026-09-24 21:31:51 +0530

    Add recursive factorial program

---
+++ Introduction/Recursion/factorial.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class factorial {
+    public static int factorial(int n){
+    if(n==1){
+        return 1;
+    }
+    return n*factorial(n-1);
+  }
+  public static void main(String[] args) {
+  System.out.print(factorial(5));
+}
+}
